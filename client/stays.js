a786b9e34c65356f0230f6d7c00122ae32bb43fd
chngpng <pengcheng85@gmail.com>
2016-04-30 13:47:35 -0400

    include maps and show marks for every stay offer

--- client/stays.js
+++ client/stays.js
@@ -5,8 +5,43 @@ Template.stays.helpers({
 
     dateFormat(date) {
       return new Date(date).toDateString()
+    },
+
+    geolocationError: function () {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+
+    mapOptions: function () {
+      var latLng = Geolocation.latLng();
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          //center: new google.maps.LatLng(40.7128, -74.0059),
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: 10
+        };
+      }
     }
 })
+
+Template.stays.onCreated(function () {
+  GoogleMaps.ready('map', function (map) {
+    //var latLng = Geolocation.latLng();
+
+    //var marker = new google.maps.Marker({
+    //  position: new google.maps.LatLng(latLng.lat, latLng.lng),
+    //  map: map.instance
+    //});
+
+    Stays.find().forEach(function (e) {
+      var geopoint = e.address.geopoint;
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(geopoint[1], geopoint[0]),
+        map: map.instance
+      });
+    });
+  });
+});
 
 Template.stays.events({
   'click .js-add-stay': function () {
@@ -16,6 +51,21 @@ Template.stays.events({
       return
     }
     $('#stayModal').modal('show');
+  },
+
+  'click .stay-card': function (e) {
+    console.log('stay card is clicked');
+    var docId = e.currentTarget.getAttribute("doc_id");
+    console.log(docId);
+    var stay = Stays.findOne({
+      _id: docId
+    });
+    console.log(stay);
+    //if (!Meteor.user()) {
+    //  alert("Log in to post offers.");
+    //  return
+    //}
+    //$('#stayModal').modal('show');
   },
 })
 
